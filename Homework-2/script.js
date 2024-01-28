27b99fb3826f6887ed8216ef397f6b5e44665da2
SpaceDad777 <gwenithv@gmail.com>
2024-01-28 16:06:37 -0700

    Create script.js

---
+++ Homework-2/script.js
@@ -0,0 +1,20 @@
+/*
+Creator: Gwenith VanGeystel
+File Name: Week 2 homework
+Date 1/28/23
+*/
+
+//
+
+console.log("JavaScript Rules");
+
+//3 favorite websites
+console.log('Youtube, Crunchyroll, Artstation');
+
+//3 favorite games
+console.log('Deep Rock Galactic, Devil May Cry, Halo');
+
+//3 favorite artists
+console.log('Vincent Van Gogh- Dutch post-impressionist painter');
+console.log('Miles Davis - Jazz trumpet player and composer, key figure in the  devlopment of jazz')
+console.log('Shigeru Miyamoto - Creator of Super Mario, a masterclass of level design')
